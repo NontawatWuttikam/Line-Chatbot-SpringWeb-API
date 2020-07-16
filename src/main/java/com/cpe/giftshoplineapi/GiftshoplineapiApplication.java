74b82b5022061b548ca3530b28f232e832d67caa
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-07-16 23:55:55 +0700

    bring back project backbone

---
+++ src/main/java/com/cpe/giftshoplineapi/GiftshoplineapiApplication.java
@@ -0,0 +1,13 @@
+package com.cpe.giftshoplineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftshoplineapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftshoplineapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpe/giftshoplineapi/GiftshoplineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.giftshoplineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftshoplineapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
