d7248b7e63f35924b98ea16bb44bb7c1b63c522f
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-07-18 00:46:59 +0700

    add getProduct Methods in productService

--- src/main/java/com/model/Product.java
+++ src/main/java/com/model/Product.java
@@ -25,4 +25,51 @@ public class Product {
     @Column(name = "IMAGEURL")
     private String imageURL;
 
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStorePageUrl() {
+        return storePageUrl;
+    }
+
+    public void setStorePageUrl(String storePageUrl) {
+        this.storePageUrl = storePageUrl;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
--- src/main/java/com/service/ProductService.java
+++ src/main/java/com/service/ProductService.java
@@ -1,7 +1,56 @@
 package com.service;
 
+import com.model.Product;
+import com.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public String getAllProductLineMessage() {
+        String msg = "รายชื่อสินค้าของร้านทั้งหมด\n";
+        List<Product> productList;
+        List<String> productStr;
+        productList = this.getAllProductList();
+        productStr = productList.stream().map( o-> {
+             String productstr = o.getProductName();
+             if (o.getQuantity() == 0)
+                productstr = o.getProductName() + "(หมด)";
+            return productstr;}).collect(Collectors.toList());
+
+        for (String productString : productStr) {
+            msg += productString + "\n";
+        }
+
+        msg = "กรุณาพิมพ์หมายเลขด้านหน้าสินค้าเพื่อดูข้อมูล";
+        return msg;
+    }
+
+    public List<Product> getAllProductList() {
+        List<Product> productList;
+        productList = productRepository.findAll();
+        return productList;
+    }
+
+    public String getSpecificProduct(Integer index) {
+        String productInfo = "";
+        List<Product> productList = getAllProductList();
+        if (index > productList.size())
+            return "ไม่พบสินค้าที่คุณร้องขอ กรุณาเลือกใหม่อีกครั้ง";
+
+        Product product = productList.get(index-1);
+        String productDescription = (product.getDescription() != null)? product.getDescription() : "";
+        productInfo = product.getProductName() + "\n\n" +
+                      productDescription + "\n\n" +
+                      "จำนวนในคลัง : " + product.getQuantity();
+        return productInfo;
+
+    }
 }
