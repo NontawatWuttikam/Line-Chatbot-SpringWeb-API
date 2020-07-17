f7852a5bc533aed41541fb9905b81fa0f892afcc
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-07-17 23:42:37 +0700

    Add lineSpringBoot dependency and callback method in mainclass

--- src/main/java/com/cpe/giftshoplineapi/GiftshoplineapiApplication.java
+++ src/main/java/com/cpe/giftshoplineapi/GiftshoplineapiApplication.java
@@ -1,13 +1,27 @@
 package com.cpe.giftshoplineapi;
 
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@LineMessageHandler
 public class GiftshoplineapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiftshoplineapiApplication.class, args);
+	}
+
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+		System.out.println("event: " + e);
+		TextMessageContent message = e.getMessage();
+		return new TextMessage(message.getText());
 	}
 
 }
