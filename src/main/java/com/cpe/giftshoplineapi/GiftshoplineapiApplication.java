57175d4bc275aa4b953fe97e5738e2a630add633
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-07-22 22:20:15 +0700

    Add firebase dependency and config class & exclude Datasource config

--- src/main/java/com/cpe/giftshoplineapi/GiftshoplineapiApplication.java
+++ src/main/java/com/cpe/giftshoplineapi/GiftshoplineapiApplication.java
@@ -9,10 +9,17 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @LineMessageHandler
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class GiftshoplineapiApplication {
 
 	public static void main(String[] args) {
